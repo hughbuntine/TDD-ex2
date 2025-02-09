c532678307fb5b8a6559b225c328fecfa0bc2218
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-09 15:46:50 +0200

    created a window attribute (pretty much the board)

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,19 +1,18 @@
 export class Board {
   width;
   height;
+  window;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.window = new Array(this.height).fill(null).map(() => new Array(this.width).fill("."));
   }
 
   toString() {
     let result = "";
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        result += ".";
-      }
-      result += "\n";
+    for (let row of this.window) {
+      result += row.join("") + "\n";
     }
     
     return result;
