6c036252913b383e306ba080e3d90fa8513c5414
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-09 16:58:21 +0200

    prints the board

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,16 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = new Array(height).fill().map(() => new Array(width).fill("."));
   }
 
   toString() {
-    return "TODO";
+    let result = this.board.map(row => row.join("")).join("\n")
+    return result + "\n";
   }
 }
