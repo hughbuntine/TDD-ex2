50657dbb353804520c1ad4d6ff0cc0f0c9cca3c9
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-09 17:07:00 +0200

    Update Board.mjs

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,11 +2,13 @@ export class Board {
   width;
   height;
   board;
+  fallingBlockPos;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.board = new Array(height).fill().map(() => new Array(width).fill("."));
+    this.fallingBlockPos = null
   }
 
   toString() {
@@ -17,6 +19,7 @@ export class Board {
   drop(block) {
     if (this.canDrop()) {
       this.board[0][Math.floor(this.width / 2)] = block;
+      this.fallingBlockPos = {x: Math.floor(this.width / 2), y: 0};
     } 
     else {
       throw new Error("DEAD I THINK");
