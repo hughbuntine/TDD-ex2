dd61fc75c79b79b10e4992101836e7f700bac63b
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-11 13:56:17 +0200

    created RotatingShape Class

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,14 @@
+class RotatingShape {
+    constructor() {
+        // Add constructor logic here
+    }
+
+    fromString (shapeString) {
+        // Add logic here
+    }
+
+    toString () {
+        // Add logic here
+    }
+}
+export default RotatingShape;
