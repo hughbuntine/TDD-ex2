ef70bee886730f5062e052ed87e505ef62e124a3
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-11 15:03:00 +0200

    rotate right

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -5,11 +5,17 @@ export class RotatingShape {
 
     static fromString (shapeString) {
         const shape = new RotatingShape();
-        shape.shape = shapeString.split('\n').map(row => row.split(''));
+        shape.shape = shapeString.split('\n').map(row => row.trim().split(''));
         return shape;
     }
 
     toString () {
-        return this.shape.map(row => row.join('')).join('\n').replaceAll(' ', '') + "\n";
+        return this.shape.map(row => row.join('')).join('\n') + "\n";
+    }
+
+    rotateRight () {
+        const rotated = new RotatingShape();
+        rotated.shape = this.shape[0].map((_, i) => this.shape.map(row => row[i]).reverse());
+        return rotated;
     }
 }
