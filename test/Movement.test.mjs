3741985868f41cd7f27eb8766fa7fae901139d52
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-12 16:40:01 +0200

    bottom test

--- test/Movement.test.mjs
+++ test/Movement.test.mjs
@@ -89,5 +89,27 @@ describe("Moving tetrominoes", () => {
         );
     });
 
+    test("it cannot be moved down beyond the board", () => {
+        board.drop(Tetromino.T_SHAPE);
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+        board.moveDown();
+
+        expect(board.toString()).to.equalShape(
+            `..........
+             ..........
+             ..........
+             ..........
+             ....T.....
+             ...TTT....`
+        );
+
 });
 
